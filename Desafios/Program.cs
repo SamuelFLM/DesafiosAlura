33a211662958d8ecfd8f8b59ccaed9f86c2f3d3e
SamuelFLM <felipelimaaa23@gmail.com>
2022-04-26 11:42:17 -0300

    Frist Commit

---
+++ Desafios/Program.cs
@@ -0,0 +1,23 @@
+﻿
+//Desafio multiplos de 3
+// for (var i = 1; i <= 100; i++)
+// {
+//     if (i % 3 == 0)
+//         Console.WriteLine(i);
+// }
+// // ou 
+// for (int i = 3; i < 100; i += 3)
+// {
+//     Console.WriteLine(i);
+// }
+
+
+
+//Desafio Fatorial
+// var resultTotal = 1;
+// for (var i = 1; i <= 10; i++)
+// {
+//     resultTotal *= i;
+//     Console.WriteLine($"O fatorial de {i} é ({i - 1}!) * {i} = {resultTotal}");
+// }
+
